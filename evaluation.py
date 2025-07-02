f76c80984a0a91c75d766f2e4058c934d5fd11a5
Aidan180804 <aidanswaine13@gmail.com>
2025-07-02 14:50:47 +0100

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,16 @@
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+#predict
+ypred = model.predict(xtest)
+ypred = le.inverse_transform(ypred)
+ypred = pd.DataFrame(ypred)
+
+guess_home_team = pd.DataFrame(['H']*76)
+
+print("Accuracy:", accuracy_score(ytest, ypred))
+print("Accuracy:", accuracy_score(ytest, guess_home_team))
+print(confusion_matrix(ytest, ypred))
+print(classification_report(ytest, ypred))
+print(combined_stats)
+
 
